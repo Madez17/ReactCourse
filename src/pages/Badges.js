ed01ad05733423fd694256bea6f18b56ad9da611
Madez17 <mafe.mendez.m@gmail.com>
2020-05-02 15:19:20 -0500

    Automatic Updates

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -20,7 +20,13 @@ class Badges extends React.Component {
     }
 
     componentDidMount () {
-        this.fetchData()
+        this.fetchData();
+
+        this.intervalId = setInterval (this.fetchData, 5000);
+    }
+
+    componentWillUnmount () {
+        clearInterval(this.intervalId)
     }
 
     fetchData = async () => {
@@ -36,7 +42,7 @@ class Badges extends React.Component {
 
     render(){
 
-        if (this.state.loading === true) {
+        if (this.state.loading === true && !this.state.data) {
             return (
                 <PageLoading />
             );
@@ -68,6 +74,8 @@ class Badges extends React.Component {
                 <div className="Badges__list">
                     <div className="Badges__container">
                         <BadgesList badges={this.state.data}/>
+
+                        {this.state.loading && 'Loading...'}
                     </div>
                 </div>
             </React.Fragment>
